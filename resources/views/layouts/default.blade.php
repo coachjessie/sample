da1107aee180dde53966f87638de4478fa2e53db
coachjessie <coachjessie@gapp.nthu.edu.tw>
2018-12-01 18:27:17 +0000

    Filling Home Page Style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,29 @@
 <html>
 <head>
     <title>@yield('title','Sample') - Laravel 新手入門</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <header class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                <a href="/" id="logo">Sample App</a>
+                <nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/help">支援</a></li>
+                        <li><a href="#">登入</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主頁</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你現在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.5"> Laravel 入門教學</a> 的範例首頁
+        </p>
+        <p>
+            一切，將從這裡開始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">現在註冊</a>
+        </p>
+    </div>
+
 @stop
